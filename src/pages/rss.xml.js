776a485ef7f43f1eb08721cd87e53253077ee8d9
pauPuigsubira <p.puigsubira.berenguer@gmail.com>
2026-07-01 08:26:48 +0200

    Set RSS Feed to share our post and first steps to interactivity with preact and Vanilla JavaScript

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -7,5 +7,7 @@ export async function GET(context) {
     site: context.site,
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>en-us</language>`,
+    // ex. use your stylesheet from "public/rss/styles.xsl"
+    stylesheet: '/rss/styles.xsl',
   });
 }
